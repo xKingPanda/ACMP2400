27b6aebe24f1216820965284c93cb2a02ce2e4af
xKingPanda <71983508+xKingPanda@users.noreply.github.com>
2026-04-25 02:14:05 -0500

    Update main.tf

--- stage1/main.tf
+++ stage1/main.tf
@@ -1,1 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "michaelfuller-acr" {
+  name = "acrmichaelfulleracmp2400"
+  resource_group_name = "rg-michaelfuller"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
