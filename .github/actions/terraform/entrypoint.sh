2fe353ad8aad09df619cd0c217e38c6df75fcd97
xKingPanda <71983508+xKingPanda@users.noreply.github.com>
2026-04-25 20:45:23 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -8,7 +8,13 @@ export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
 export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
+export DJANGO_SECRET_KEY_PROD=${DJANGO_SECRET_KEY_PROD}
 
-tofu -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-tofu -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-tofu -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+if [ "$TF_STAGE" = "stage1" ]; then 
+  tofu -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+  tofu -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+  tofu -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+elif [ "$TF_STAGE" = "stage2" ]; then
+  tofu -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+  tofu -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
+fi
