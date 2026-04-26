10605a228cf176f43ad7b4a1b372ba2bcbafb595
xKingPanda <71983508+xKingPanda@users.noreply.github.com>
2026-04-26 03:01:01 -0500

    Create main.tf

---
+++ stage3/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
