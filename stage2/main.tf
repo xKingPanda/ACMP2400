23135593caed945388e8c358941114746f33d630
xKingPanda <71983508+xKingPanda@users.noreply.github.com>
2026-04-25 19:41:33 -0500

    Create main.tf

---
+++ stage2/main.tf
@@ -0,0 +1,63 @@
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "DJANGO_SECRET_KEY_PROD" {}
+
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "student-acr" {
+  name = "acrmichaelfulleracmp2400"
+  resource_group_name = "rg-michaelfuller"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
+
+resource "azurerm_container_group" "michaelfuller-aci" {
+  name = "acmp-michaelfuller-aci"
+  location = "Central US"
+  resource_group_name = "rg-michaelfuller"
+  ip_address_type = "Public"
+  dns_name_label = "acmp-michaelfuller-instance"
+  os_type = "Linux"
+
+  container {
+    name = "final"
+    image = "acrmichaelfulleracmp2400.azurecr.io/final:latest"
+    cpu = "0.5"
+    memory = "1.5"
+
+  ports {
+    port = 8000
+    protocol = "TCP"
+    }
+
+    secure_environment_variables = {
+      DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
+    }
+  }
+    image_registry_credential {
+      server = "acrmichaelfulleracmp2400.azurecr.io"
+      username = var.ARM_CLIENT_ID
+      password = var.ARM_CLIENT_SECRET
+
+    }
+  }
